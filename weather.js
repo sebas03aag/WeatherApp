2a957dc6204a45fa886815b95c8d3ba363c00043
sebas03aag <sebas03aag@gmail.com>
2023-06-30 17:07:10 -0600

    Project Added

---
+++ weather.js
@@ -0,0 +1,56 @@
+const apiKey = "89d2d99de2f0d7e19b5bc78bf5aa66c3";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }else{
+        var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "clouds.png";
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "clear.png";
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "rain.png";
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "mist.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+
+
+    }
+
+}
+
+searchBtn.addEventListener ("click", ()=>{
+    checkWeather(searchBox.value);
+})
+      searchBox.addEventListener("keypress", (event) => {
+        // If the user presses the "Enter" key on the keyboard
+        if (event.key === "Enter") {
+          // Trigger the button element with a click
+          document.querySelector(".myBtn").click();
+        }
+      });
